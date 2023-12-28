ce8db0843b747c83afae8136d67ff2b52c314cee
BorislavDimitrov <86628373+BorislavDimitrov@users.noreply.github.com>
2023-12-28 23:41:16 +0200

    Create UserInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <UserInput />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,26 @@
+export default function UserInput() {
+  return (
+    <div id="user-input">
+      <div className="input-group">
+        <p>
+          <label for="fname">Initial investment</label>
+          <input type="text" name="fname" required />
+        </p>
+        <p>
+          <label for="fname">Annual investment</label>
+          <input type="text" name="fname" required />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label for="fname">Expected return</label>
+          <input type="text" name="fname" required />
+        </p>
+        <p>
+          <label for="fname">Duration</label>
+          <input type="number" name="fname" required min="1" />
+        </p>
+      </div>
+    </div>
+  );
+}
